9ab8752b55e34f8e25e99f7e92113c2107541020
Keith Philpott <keith.philpott@icloud.com>
2022-10-23 14:38:23 -0700

    wip

--- codegen/dict_validator.py
+++ codegen/dict_validator.py
@@ -128,7 +128,7 @@ class MapValidator(Validator[Any, Dict[T1, T2], JSONValue]):
             else:
                 return Ok(return_dict)
         else:
-            return Err({"OBJECT_ERRORS_FIELD": [expected("a map")]})
+            return Err({OBJECT_ERRORS_FIELD: [expected("a map")]})
 
 
 class IsDict(Validator[Any, Dict[Any, Any], JSONValue]):
