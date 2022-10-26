00eec9c088f2a1eca6d96f253505c1cc724a1021
Keith Philpott <keith.philpott@icloud.com>
2022-10-26 14:41:31 -0700

    wip

--- koda_validate/utils.py
+++ koda_validate/utils.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Callable, Iterable, List, Optional, cast
+from typing import Callable, Iterable, List, Optional
 
 from koda import Err, Ok, Result
 
@@ -16,13 +16,10 @@ def accum_errors(
     val: A, validators: Iterable[Predicate[A, FailT]]
 ) -> Result[A, List[FailT]]:
     errors: List[FailT] = []
-    # result: Result[A, FailT] = Ok(val)
     for validator in validators:
         result = validator(val)
         if isinstance(result, Err):
             errors.append(result.val)
-        # else:
-        # val = result.val
 
     if len(errors) > 0:
         return Err(errors)
@@ -42,20 +39,6 @@ def accum_errors_serializable(
 
 def _variant_errors(*variants: Serializable) -> Serializable:
     return {f"variant {i + 1}": v for i, v in enumerate(variants)}
-
-
-def _flat_map_same_type_if_not_none(
-    fn: Optional[Callable[[A], Result[A, FailT]]],
-    r: Result[A, FailT],
-) -> Result[A, FailT]:
-    if fn is None:
-        return r
-    else:
-        # optimizing by not using flatmap
-        if isinstance(r, Err):
-            return r
-        else:
-            return fn(r.val)
 
 
 OBJECT_ERRORS_FIELD = "__container__"
