87afd4f6db856fc2e3d32fe1915c45994b787713
Keith Philpott <keith.philpott@icloud.com>
2022-10-27 08:56:53 -0700

    added bench

---
+++ bench/one_key_invalid_types.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+
+from koda import Err, Ok
+from pydantic import BaseModel, ValidationError, constr
+
+from koda_validate import StringValidator, key
+from koda_validate.dictionary import dict_validator
+
+
+@dataclass
+class SimpleStr:
+    val_1: str
+
+
+string_validator = dict_validator(SimpleStr, key("val_1", StringValidator()))
+
+
+def run_kv(iterations: int) -> None:
+    for i in range(iterations):
+        match string_validator({"val_1": i}):
+            case Ok(val):
+                x = val
+            case Err(val):
+                x = val
+
+
+class BasicString(BaseModel):
+    val_1: constr(strict=True)
+
+
+def run_pyd(iterations: int) -> None:
+    for i in range(iterations):
+        try:
+            BasicString(val_1=i)
+        except ValidationError as e:
+            x = e
---
+++ bench/run.py
@@ -0,0 +1,58 @@
+from argparse import ArgumentParser
+from dataclasses import dataclass
+from time import perf_counter
+from typing import Callable
+
+from bench import one_key_invalid_types, two_keys_invalid_types, two_keys_valid
+
+
+@dataclass
+class BenchCompare:
+    kv_run: Callable[[int], None]
+    pyd_run: Callable[[int], None]
+
+
+benches = {
+    "one_key_invalid_types": BenchCompare(
+        one_key_invalid_types.run_kv, one_key_invalid_types.run_pyd
+    ),
+    "two_keys_invalid_types": BenchCompare(
+        two_keys_invalid_types.run_kv, two_keys_invalid_types.run_pyd
+    ),
+    "two_keys_valid": BenchCompare(two_keys_valid.run_kv, two_keys_valid.run_pyd),
+}
+
+
+def run_bench(iterations: int, fn: Callable[[int], None]) -> None:
+    start = perf_counter()
+    fn(iterations)
+    t_ = perf_counter() - start
+    print(f"Execution time: {t_:.4f} secs\n")
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "tests",
+        type=str,
+        nargs="*",
+        help="which tests to run",
+    )
+    parser.add_argument(
+        "--iterations",
+        type=int,
+        default=100_000,
+        help="How many iterations of each test we'll run",
+    )
+    args = parser.parse_args()
+
+    print(f"{args.iterations} ITERATIONS")
+
+    for name, compare_bench in benches.items():
+        if args.tests == [] or name in args.tests:
+            print(f"----- BEGIN {name} -----\n")
+            print("KODA_VALIDATE")
+            run_bench(args.iterations, compare_bench.kv_run)
+            print("PYDANTIC")
+            run_bench(args.iterations, compare_bench.pyd_run)
+            print(f"----- END {name} -----\n")
