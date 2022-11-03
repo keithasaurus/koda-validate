876be1f9a0c40a53297c584402c45976e4d979ac
Keith Philpott <keith.philpott@icloud.com>
2022-11-02 23:57:11 -0700

    wip

--- examples/float_validator.py
+++ examples/float_validator.py
@@ -1,20 +1,20 @@
-from typing import Any
-
-from koda import Err, Ok, Result
-
-from koda_validate import Predicate, Serializable, Validator
-from koda_validate.utils import accum_errors
-
-
-class FloatValidator(Validator[Any, float, Serializable]):
-    def __init__(self, *predicates: Predicate[float, Serializable]) -> None:
-        self.predicates = predicates
-
-    def __call__(self, val: Any) -> Result[float, Serializable]:
-        if isinstance(val, float):
-            if len(self.predicates) > 0:
-                return accum_errors(val, self.predicates)
-            else:
-                return Ok(val)
-        else:
-            return Err(["expected a float"])
+# from typing import Any
+#
+# from koda import Err, Ok, Result
+#
+# from koda_validate import Predicate, Serializable, Validator
+# from koda_validate.utils import accum_errors
+#
+#
+# class FloatValidator(Validator[Any, float, Serializable]):
+#     def __init__(self, *predicates: Predicate[float, Serializable]) -> None:
+#         self.predicates = predicates
+#
+#     def __call__(self, val: Any) -> Result[float, Serializable]:
+#         if isinstance(val, float):
+#             if len(self.predicates) > 0:
+#                 return accum_errors(val, self.predicates)
+#             else:
+#                 return Ok(val)
+#         else:
+#             return Err(["expected a float"])
--- koda_validate/utils.py
+++ koda_validate/utils.py
@@ -6,45 +6,6 @@ from koda_validate._generics import A, FailT
 from koda_validate.typedefs import Predicate, PredicateAsync, Processor, Serializable
 
 
-def _handle_accum(val: A, errors: List[Serializable]) -> Result[A, Serializable]:
-    if len(errors) > 0:
-        return Err(errors)
-    else:
-        # has to be original val because there are no
-        # errors, and predicates prevent there from being
-        # modification to the value
-        return Ok(val)
-
-
-def accum_errors(
-    val: A, validators: Tuple[Predicate[A, Serializable], ...]
-) -> Result[A, Serializable]:
-    return _handle_accum(
-        val,
-        [
-            result.val
-            for validator in validators
-            if isinstance(result := validator(val), Err)
-        ],
-    )
-
-
-async def accum_errors_async(
-    val: A,
-    predicates: Tuple[Predicate[A, Serializable], ...],
-    predicates_async: List[PredicateAsync[A, Serializable]],
-) -> Result[A, Serializable]:
-    return _handle_accum(
-        val,
-        [result.val for pred in predicates if isinstance(result := pred(val), Err)]
-        + [
-            result.val
-            for pred in predicates_async
-            if isinstance(result := await pred.validate_async(val), Err)
-        ],
-    )
-
-
 def _variant_errors(*variants: Serializable) -> Serializable:
     return {f"variant {i + 1}": v for i, v in enumerate(variants)}
 
@@ -102,7 +63,7 @@ async def _handle_scalar_processors_and_predicates_async(
 
     errors = []
 
-    if len(predicates) > 0:
+    if len(predicates) != 0:
         errors.extend(
             [result.val for pred in predicates if isinstance(result := pred(val), Err)]
         )
@@ -114,20 +75,13 @@ async def _handle_scalar_processors_and_predicates_async(
                 if isinstance(result := await pred.validate_async(val), Err)
             ]
         )
-    if len(errors) > 0:
+    if len(errors) != 0:
         return Err(errors)
     else:
         # has to be original val because there are no
         # errors, and predicates prevent there from being
         # modification to the value
         return Ok(val)
-
-
-SCALAR_SYNC_ASYNC_SLOTS_AND_MATCH_ARGS: Final[Tuple[str, ...]] = (
-    "predicates",
-    "predicates_async",
-    "preprocessors",
-)
 
 
 class _NotSet:
