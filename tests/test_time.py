651ed090b3dc03acee5bb3cdc60c3f282ce7b096
Keith Philpott <keith.philpott@icloud.com>
2022-11-03 16:14:25 -0700

    tests

--- tests/test_time.py
+++ tests/test_time.py
@@ -1,5 +1,6 @@
 from datetime import date, datetime
 
+import pytest
 from koda import Err, Ok
 
 from koda_validate import DateStringValidator, DatetimeStringValidator
@@ -12,9 +13,32 @@ def test_date_validator() -> None:
     )
 
 
+@pytest.mark.asyncio
+async def test_date_validator_async() -> None:
+    assert await DateStringValidator().validate_async("2021-03-21") == Ok(
+        date(2021, 3, 21)
+    )
+    assert await DateStringValidator().validate_async("2021-3-21") == Err(
+        ["expected date formatted as yyyy-mm-dd"]
+    )
+
+
 def test_datetime_validator() -> None:
     assert DatetimeStringValidator()("") == Err(["expected iso8601-formatted string"])
     assert DatetimeStringValidator()("2011-11-04") == Ok(datetime(2011, 11, 4, 0, 0))
     assert DatetimeStringValidator()("2011-11-04T00:05:23") == Ok(
+        datetime(2011, 11, 4, 0, 5, 23)
+    )
+
+
+@pytest.mark.asyncio
+async def test_datetime_validator_async() -> None:
+    assert await DatetimeStringValidator().validate_async("") == Err(
+        ["expected iso8601-formatted string"]
+    )
+    assert await DatetimeStringValidator().validate_async("2011-11-04") == Ok(
+        datetime(2011, 11, 4, 0, 0)
+    )
+    assert await DatetimeStringValidator().validate_async("2011-11-04T00:05:23") == Ok(
         datetime(2011, 11, 4, 0, 5, 23)
     )
--- tests/test_tuple.py
+++ tests/test_tuple.py
@@ -1,5 +1,6 @@
 from typing import Tuple
 
+import pytest
 from koda import Err, Ok, Result
 
 from koda_validate import BoolValidator, IntValidator, Serializable, StringValidator
@@ -94,3 +95,56 @@ def test_tuple3() -> None:
         {"__container__": ["must be a if int is 1 and bool is True"]}
     )
     assert a1_validator(["b", 2, False]) == Ok(("b", 2, False))
+
+
+@pytest.mark.asyncio
+async def test_tuple3_async() -> None:
+    assert await Tuple3Validator(
+        StringValidator(), IntValidator(), BoolValidator()
+    ).validate_async({}) == Err({"__container__": ["expected list or tuple of length 3"]})
+
+    assert await Tuple3Validator(
+        StringValidator(), IntValidator(), BoolValidator()
+    ).validate_async([]) == Err({"__container__": ["expected list or tuple of length 3"]})
+
+    assert await Tuple3Validator(
+        StringValidator(), IntValidator(), BoolValidator()
+    ).validate_async(["a", 1, False]) == Ok(("a", 1, False))
+
+    assert await Tuple3Validator(
+        StringValidator(), IntValidator(), BoolValidator()
+    ).validate_async(("a", 1, False)) == Ok(("a", 1, False))
+
+    assert await Tuple3Validator(
+        StringValidator(), IntValidator(), BoolValidator()
+    ).validate_async([1, "a", 7.42]) == Err(
+        {
+            "0": ["expected a string"],
+            "1": ["expected an integer"],
+            "2": ["expected a boolean"],
+        }
+    )
+
+    def must_be_a_if_1_and_true(
+        abc: Tuple[str, int, bool]
+    ) -> Result[Tuple[str, int, bool], Serializable]:
+        if abc[1] == 1 and abc[2] is True:
+            if abc[0] == "a":
+                return Ok(abc)
+            else:
+                return Err({"__container__": ["must be a if int is 1 and bool is True"]})
+        else:
+            return Ok(abc)
+
+    a1_validator = Tuple3Validator(
+        StringValidator(),
+        IntValidator(),
+        BoolValidator(),
+        must_be_a_if_1_and_true,
+    )
+
+    assert await a1_validator.validate_async(["a", 1, True]) == Ok(("a", 1, True))
+    assert await a1_validator.validate_async(["b", 1, True]) == Err(
+        {"__container__": ["must be a if int is 1 and bool is True"]}
+    )
+    assert await a1_validator.validate_async(["b", 2, False]) == Ok(("b", 2, False))
