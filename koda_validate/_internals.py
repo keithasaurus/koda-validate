b339c30f2cf7844cac6aba545ae3e38829406ccf
Keith Philpott <keith.philpott@icloud.com>
2022-11-07 10:21:53 -0800

    fix long line

--- koda_validate/_internals.py
+++ koda_validate/_internals.py
@@ -119,5 +119,6 @@ _not_set = _NotSet()
 def _async_predicates_warning(cls: Type[Any]) -> NoReturn:
     raise AssertionError(
         f"{cls.__name__} cannot run `predicates_async` in synchronous calls. "
-        f"Please remove the items in `predicates_async`, or call the `.validate_async` method instead."
+        f"Please remove the items in `predicates_async`, or call the `.validate_async` "
+        f"method instead."
     )
