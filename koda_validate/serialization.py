fe73204497b7d7c9baebc3ee80c055de89466a9b
Keith Philpott <keith.philpott@icloud.com>
2022-11-24 08:48:15 -0800

    don't use match

--- koda_validate/serialization.py
+++ koda_validate/serialization.py
@@ -16,35 +16,33 @@ from koda_validate.base import (
 
 
 def serializable_validation_err(err: ValidationErr) -> Serializable:
-    match err:
-        case InvalidCoercion(_, _, err_message):
-            return [err_message]
-        case InvalidCustom(err_message):
-            return [err_message]
-        case InvalidExtraKeys(_, err_message):
-            return [err_message]
-        case InvalidType(_, err_message):
-            return [err_message]
-        case [*predicates]:
-            return [p.err_message for p in predicates]
-        case InvalidIterable(errs):
-            return [[i, serializable_validation_err(err)] for i, err in errs.items()]
-        case InvalidDict(key_errs):
-            return {str(k): serializable_validation_err(v) for k, v in key_errs.items()}
-        case InvalidKeyMissing():
-            return ["key missing"]
-        case InvalidMap(key_val_errs):
-            errs_dict: Dict[str, Serializable] = {}
-            for key, k_v_errs in key_val_errs.items():
-                kv_dict: Dict[str, Serializable] = {
-                    k: serializable_validation_err(v)
-                    for k, v in [("key", k_v_errs.key), ("value", k_v_errs.val)]
-                    if v is not None
-                }
-                errs_dict[str(key)] = kv_dict
-            return errs_dict
-        case InvalidVariants(variants):
-            return {"variants": [serializable_validation_err(x) for x in variants]}
-
-        case _:
-            raise TypeError(f"got unhandled type: {type(err)}")
+    if isinstance(err, InvalidCoercion):
+        return [err.err_message]
+    elif isinstance(err, InvalidCustom):
+        return [err.err_message]
+    elif isinstance(err, InvalidExtraKeys):
+        return [err.err_message]
+    elif isinstance(err, InvalidType):
+        return [err.err_message]
+    elif isinstance(err, list):
+        return [p.err_message for p in err]
+    elif isinstance(err, InvalidIterable):
+        return [[i, serializable_validation_err(err)] for i, err in err.indexes.items()]
+    elif isinstance(err, InvalidKeyMissing):
+        return ["key missing"]
+    elif isinstance(err, InvalidMap):
+        errs_dict: Dict[str, Serializable] = {}
+        for key, k_v_errs in err.keys.items():
+            kv_dict: Dict[str, Serializable] = {
+                k: serializable_validation_err(v)
+                for k, v in [("key", k_v_errs.key), ("value", k_v_errs.val)]
+                if v is not None
+            }
+            errs_dict[str(key)] = kv_dict
+        return errs_dict
+    elif isinstance(err, InvalidDict):
+        return {str(k): serializable_validation_err(v) for k, v in err.keys.items()}
+    elif isinstance(err, InvalidVariants):
+        return {"variants": [serializable_validation_err(x) for x in err.variants]}
+    else:
+        raise TypeError(f"got unhandled type: {type(err)}")
