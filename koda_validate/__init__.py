7ec0a77dd9c6e8df6c3e07c9e42177e8c746ff5a
Keith Philpott <keith.philpott@graphitehq.com>
2022-12-07 16:07:41 -0800

    init imports

--- koda_validate/__init__.py
+++ koda_validate/__init__.py
@@ -64,9 +64,11 @@ __all__ = (
     # one_of.py
     "OneOf2",
     "OneOf3",
-    # serialization
+    # serialization.py
     "Serializable",
     "serializable_validation_err",
+    # set.py
+    "SetValidator",
     # string.py
     "StringValidator",
     "MinLength",
@@ -147,6 +149,7 @@ from koda_validate.list import ListValidator
 from koda_validate.none import NoneValidator, OptionalValidator, none_validator
 from koda_validate.one_of import OneOf2, OneOf3
 from koda_validate.serialization import Serializable, serializable_validation_err
+from koda_validate.set import SetValidator
 from koda_validate.string import (
     EmailPredicate,
     MaxLength,
