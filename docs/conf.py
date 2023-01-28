1546e60167f0dde20b030a43137f106933941843
Keith Philpott <keith.philpott@icloud.com>
2023-01-28 10:07:54 -0800

    fix docs version

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ from typing import List
 project = "Koda Validate"
 copyright = "2023, Keith Philpott"
 author = "Keith Philpott"
-release = "3.1.0"
+release = "3.1.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
